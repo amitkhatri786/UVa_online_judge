80a4343cdd0c07eb548db9778e12bcde79105b61
Amit Khatri <amit7861234@gmail.com>
2017-06-22 19:05:20 +0530

    Changes final , code submitted

--- GRAPH/CONNECTED_COMPONENT/connected.c
+++ GRAPH/CONNECTED_COMPONENT/connected.c
@@ -2,9 +2,9 @@
 #include<malloc.h>
 
 
-#define DEBUG
+//#define DEBUG
 
-#define STACK_SIZE 30000
+#define STACK_SIZE 16000000
 
 //int mat[100005][100005];
 struct graph
@@ -15,189 +15,106 @@ struct graph
 
 typedef struct graph GRAPH;
 
-GRAPH *mat[1000005] =  {0};
-GRAPH *stack[STACK_SIZE]= {0};
-int visited[100005]={0};
 
-#if 0
-int component_count(int start_vertex,int N,int *number_vertex)
+int stack[STACK_SIZE] = { 0 };
+
+
+int component_count(int start_vertex, int N,int visited[],GRAPH *mat[])
 {
 
-	int top=0;
-	int u,v;
-	int total=0;
-	stack[top]=start_vertex;
-	visited[start_vertex] = 1;
 
-	while(top>=0)
-	{
-		u = stack[top];
-		total++;
-		top--;
-		for(v=0;v<N;v++)
-		{
-			if(mat[u][v] == 1 && visited[v] !=1)
-			{
-				stack[++top] = v;
-				visited[v] = 1;
-			}
-		}
-	}
-	*number_vertex = total;
-#ifdef DEBUG
-	printf("Total vrtex in this component is %d\n",total);
-#endif
-	return(1);
-}
-#endif
-
-int component_count(int start_vertex,int N,int *number_vertex)
-{
 	int top = 0;
-	GRAPH *u,*v;
-	GRAPH *temp;
-	int total=0;
-	stack[top] = mat[start_vertex];
+	GRAPH *u, *v;
+	int temp;
+
+	stack[top] = start_vertex;
 	visited[start_vertex] = 1;
-	while(top >= 0)
+	while (top >= 0)
 	{
-		u = stack[top];
-		total++;
+		temp = stack[top];
 		top--;
-		temp = u;
-		while(temp != NULL) 
+		u = mat[temp];
+		while (u != NULL)
 		{
-			if(visited[temp->node] != 1)
+			if (visited[u->node] != 1)
 			{
-				stack[++top] = temp;
-				visited[temp->node] = 1;
+				stack[++top] = u->node;
+				visited[u->node] = 1;
 			}
-		
-			temp = temp->next;
+			u = u->next;
 		}
 	}
-	*number_vertex = total;
-#ifdef DEBUG
-	printf("Total vrtex in this component is %d\n",total);
-#endif
+
 	return(1);
 }
 
-void insert_edges(int u,int v)
+
+void insert_edges(GRAPH *mat[],int u, int v)
 {
 	GRAPH *head;
-	GRAPH *prev;
-	
-	if(mat[u-1] == NULL)
-	{
-		head = (GRAPH *)calloc(1,sizeof(GRAPH));
-		head->node = v-1;
-		head->next = NULL;
-		mat[u-1] = head;
-	}
-	else
-	{
-		head = mat[u-1];
-		while(head!=NULL)
-		{
-			prev = head;
-			head = head ->next;
-		}
-		prev->next = (GRAPH *)calloc(1,sizeof(GRAPH));
-		prev->next-> node = u-1;
-		prev->next-> next = NULL;
-	}
+	GRAPH *temp;
 
-	if(mat[v-1] == NULL)
-	{
-		head = (GRAPH *)calloc(1,sizeof(GRAPH));
-		head->node = u-1;
-		head->next = NULL;
-		mat[v-1] = head;
-	}
-	else
-	{
-		head = mat[v-1];
-		while(head!=NULL)
-		{
-			prev = head;
-			head = head ->next;
-		}
-		prev->next = (GRAPH *)calloc(1,sizeof(GRAPH));
-		prev->next->node = v-1;
-		prev->next->next = NULL;
-	}
+
+
+	head = mat[u - 1];
+	temp = (GRAPH *)calloc(1, sizeof(GRAPH));
+	temp->node = v - 1;
+	temp->next = head;
+	mat[u-1] = temp;
+
+	head = mat[v - 1];
+	temp = (GRAPH *)calloc(1, sizeof(GRAPH));
+	temp->node = u - 1;
+	temp->next = head;
+	mat[v-1] = temp;
 }
 
 
-int main() 
+int main()
 {
-	int n,m; 
-	long int C_lib,C_road; 
-	int q,a0,a1;
-	int city_1,city_2;
-	long int cost=0;
-	int i,count;
-	int component_vertex_count=0;
-	int road_cost,lib_cost;
+	int n, m;
+	long int C_lib, C_road;
+	int q, a0, a1;
+	int city_1, city_2;
+	long int cost = 0;
+	int i, count;
+	// int component_vertex_count = 0;
+	long int road_cost, lib_cost;
 	int j;
-	GRAPH *head=NULL;
+	GRAPH *head = NULL;
 
 	scanf("%i", &q);
-	for(a0 = 0; a0 < q; a0++)
+	for (a0 = 0; a0 < q; a0++)
 	{
-		cost =0;
-		count =0;
+		cost = 0;
+		count = 0;
 		scanf("%i %i %li %li", &n, &m, &C_lib, &C_road);
-		for(a1 = 0; a1 < m; a1++)
+		GRAPH *mat[100005] ={0};
+		for (a1 = 0; a1 < m; a1++)
 		{
 			scanf("%i %i", &city_1, &city_2);
-			insert_edges(city_1,city_2);
-			//mat[city_1-1][city_2-1] = 1;
-			//mat[city_2-1][city_1-1] = 1;
+			insert_edges(mat,city_1, city_2);
+			//insert_edges(mat,city_2, city_1);
 		}
-		for(i=0;i<n;i++)
+		if (C_lib < C_road)
 		{
-			if(visited[i] != 1)
-			{
-				count = count + component_count(i,n,&component_vertex_count);
-				road_cost = (component_vertex_count-1) * C_road + C_lib;
-				lib_cost  = component_vertex_count * C_lib;
-				if(road_cost < lib_cost)
-				{
-#ifdef DEBUG
-					printf("Select road repairing\n");
-#endif
-					cost = cost + road_cost;
-				}
-				else
-				{
-#ifdef DEBUG
-					printf("Select library building\n");
-#endif
-					cost = cost + (lib_cost);
-				}
-			}
-			
+			cost = n*C_lib;
 		}
-#ifdef DEBUG
-		printf("Total component count= %d\n",count);
-#endif
-		printf("%li\n",cost);
-		for(i=0;i<n;i++)
-			visited[i]=0;
-		for(i=0;i<n;i++)
+		else
 		{
-			head = mat[i];
-			while(head!=NULL)
+			int visited[100005] = { 0 };
+			for (i = 0; i<n; i++)
 			{
-				mat[i] = head->next;
-				free(head);
-				head=NULL;
+				if (visited[i] != 1)
+					count = count + component_count(i, n, visited,mat);
 			}
-			mat[i] = NULL;
-
+			cost = (n - count) *C_road + count*C_lib; //Number of edges = n - connected component
 		}
+#ifdef DEBUG
+		printf("Total component count= %d\n", count);
+#endif
+		printf("%ld\n", cost);
 	}
 	return 0;
 }
+
